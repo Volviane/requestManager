6cec5a519207c3dd43eb0be6d96f72204c1349e0
Volviane <sapphiremfogo@gmail.com>
2018-04-05 08:21:37 +0100

    Ajout des sources de requestManager

---
+++ src/main/java/com/requestManager/requestManager/RequestManagerApplication.java
@@ -0,0 +1,12 @@
+package com.requestManager.requestManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestManagerApplication.class, args);
+	}
+}
